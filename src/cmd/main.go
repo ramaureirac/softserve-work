691fe2a8e0c723a65f9ea56a41a37db36f41e932
Rodrigo Maureira Contreras <ramaureirac@pm.me>
2026-05-18 14:03:13 -0400

    feat: add gin gonic

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"log"
+
+	server "github.com/ramaureirac/softserve-work/src/server"
 )
 
 func main() {
-	log.Println("Base project uwu")
+	log.Println("Server running in http://localhost:8080")
+	srv := server.NewRouterApp()
+	srv.Run()
 }
--- src/server/router.go
+++ src/server/router.go
@@ -1,1 +1,28 @@
 package server
+
+import (
+	"net/http"
+	"os"
+
+	gin "github.com/gin-gonic/gin"
+)
+
+func NewRouterApp() *gin.Engine {
+
+	switch os.Getenv("GIN_MODE") {
+	case "testing":
+		gin.SetMode(gin.DebugMode)
+	default:
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	router := gin.New()
+	router.SetTrustedProxies([]string{"0.0.0.0"})
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]string{"code": "OK"})
+		c.Status(http.StatusOK)
+	})
+
+	return router
+}
